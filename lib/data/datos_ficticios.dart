b90c99e8ab0b7d5e7cc018bf41b1bd49a4d3f915
Byron Montaño <byronmb0407@gmail.com>
2024-04-20 21:36:49 -0500

    agregado imagenes

--- lib/data/datos_ficticios.dart
+++ lib/data/datos_ficticios.dart
@@ -9,29 +9,29 @@ var comidaDisponible = [
         'Un corte premium de carne jugosa y tierna, cocinado a la perfección y acompañado de guarniciones frescas y sabrosas.',
   ),
   Comida(
-    imagenUrl: 'assets/images/fritada.png',
+    imagenUrl: 'assets/images/salmon_limon.png',
     nombre: 'Salmon al Limon',
     precio: 6.00,
     descripcion:
         'Una explosión de sabor con trozos crujientes de cerdo sazonados, fritos hasta alcanzar un dorado perfecto y servidos con complementos deliciosos.',
   ),
   Comida(
-    imagenUrl: 'assets/images/Guatita.png',
+    imagenUrl: 'assets/images/linguine_allitaliana.png',
     nombre: 'Linguine all\'italiana',
     precio: 4.00,
     descripcion:
         ' Un platillo tradicional que combina sabores auténticos y texturas únicas, resaltando los ingredientes locales en una presentación cautivante.',
   ),
   Comida(
-    imagenUrl: 'assets/images/encebollado.png',
-    nombre: 'Encebollado',
+    imagenUrl: 'assets/images/spaguetti_camaron.png',
+    nombre: 'Spaguetti con Camarón',
     precio: 5.00,
     descripcion:
         'Una reconfortante sopa de pescado con un caldo abundante y fragante, realzado con cebollas y especias que te transportarán a la costa.',
   ),
   Comida(
-    imagenUrl: 'assets/images/hamburguesa-bbq.png',
-    nombre: 'Hamburguesa Bbq',
+    imagenUrl: 'assets/images/pastel_cafe_caramelo.png',
+    nombre: 'Pastel de Café y Caramelo',
     precio: 13.50,
     descripcion:
         'Nuestra interpretación de la hamburguesa clásica, con una generosa porción de carne jugosa y una irresistible salsa barbacoa que deleitará tus sentidos.',
