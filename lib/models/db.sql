73cdb2bd51c421fec593c3fd26089729fd3debbd
Byron Montaño <byronmb0407@gmail.com>
2024-04-26 22:38:11 -0500

    Update db.sql

--- lib/models/db.sql
+++ lib/models/db.sql
@@ -16,10 +16,16 @@ VALUES ('Patatas asadas', 'Deliciosas papas asadas al horno, tiernas por dentro 
 ('Salmón al limón]', 'Exquisito filete de salmón fresco, marinado en jugo de limón y especias selectas para resaltar su sabor natural.', 'https://i.imgur.com/LmUmNzr.png', 9.50, 200, 200, true);
 
 INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
-VALUES ('Linguine all italiana', '', 'https://i.imgur.com/s4jq0M7.png', 4.00, 400, 300, false),
-('Spaguetti con Camarón', '', 9.50, 200, 200, true);
+VALUES ('Linguine all italiana', '', 'https://i.imgur.com/s4jq0M7.png', 15.00, 800, 300, false),
+('Spaguetti con Camarón', '', 'https://i.imgur.com/2J81l39.png', 9.50, 200, 200, true);
 
+INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
+VALUES ('Pastel de Café y Caramelo', '', 'https://i.imgur.com/taWVlyI.png', 4.00, 400, 300, false),
+('Ensalada de pera y rúcula', '', 'https://i.imgur.com/W3cRDgF.png', 9.50, 200, 200, true);
 
+INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
+VALUES ('Ensalada de algas wakame', '', 'https://i.imgur.com/l8roQZ2.png', 4.00, 400, 300, false),
+('Frutos rojos', '', 'https://i.imgur.com/O6h4dj2.png', 9.50, 200, 200, true);
 
 
 
