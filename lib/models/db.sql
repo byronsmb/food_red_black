9851d7e0d7e67b39b2e35bcb91a772520686b242
Byron Montaño <byronmb0407@gmail.com>
2024-04-26 22:59:19 -0500

    Update db.sql

--- lib/models/db.sql
+++ lib/models/db.sql
@@ -16,16 +16,16 @@ VALUES ('Patatas asadas', 'Deliciosas papas asadas al horno, tiernas por dentro 
 ('Salmón al limón]', 'Exquisito filete de salmón fresco, marinado en jugo de limón y especias selectas para resaltar su sabor natural.', 'https://i.imgur.com/LmUmNzr.png', 9.50, 200, 200, true);
 
 INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
-VALUES ('Linguine all italiana', '', 'https://i.imgur.com/s4jq0M7.png', 15.00, 800, 300, false),
-('Spaguetti con Camarón', '', 'https://i.imgur.com/2J81l39.png', 9.50, 200, 200, true);
+VALUES ('Linguine all italiana', 'Pasta al dente bañada en una exquisita salsa de tomate fresco, ajo aromático y hierbas mediterráneas', 'https://i.imgur.com/s4jq0M7.png', 15.00, 800, 300, false),
+('Spaguetti con Camarón', 'Espaguetis de tinta de calamar perfectamente cocidos, envueltos en una cremosa salsa de mariscos y coronados con camarones jugosos y tiernos', 'https://i.imgur.com/2J81l39.png', 19.99, 900, 250, false);
 
 INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
-VALUES ('Pastel de Café y Caramelo', '', 'https://i.imgur.com/taWVlyI.png', 4.00, 400, 300, false),
-('Ensalada de pera y rúcula', '', 'https://i.imgur.com/W3cRDgF.png', 9.50, 200, 200, true);
+VALUES ('Pastel de Café y Caramelo', 'Bizcocho de café y dulzura de caramelo, todo en una porción', 'https://i.imgur.com/taWVlyI.png', 2.00, 200, 100, false),
+('Ensalada de pera y rúcula', 'Peras frescas, rúcula tierna y mas, todo ello aderezado con una vinagreta de miel', 'https://i.imgur.com/W3cRDgF.png', 4.99, 200, 150, false);
 
 INSERT INTO comidas (nombre,  descripcion, image_url, precio, calorias, peso, isFavorite)
-VALUES ('Ensalada de algas wakame', '', 'https://i.imgur.com/l8roQZ2.png', 4.00, 400, 300, false),
-('Frutos rojos', '', 'https://i.imgur.com/O6h4dj2.png', 9.50, 200, 200, true);
+VALUES ('Ensalada de algas wakame', 'Algas suaves wakame sazonadas con un toque de vinagre de arroz y semillas de sésamo', 'https://i.imgur.com/l8roQZ2.png', 8.00, 100, 150, false),
+('Frutos rojos', 'Una combinación perfecta de uvas, moras y cerezas para deleitar tu paladar de manera saludable y deliciosa', 'https://i.imgur.com/O6h4dj2.png', 3.50, 150, 200, true);
 
 
 
