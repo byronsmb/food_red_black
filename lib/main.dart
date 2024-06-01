fd2b1c0f4ea60fd7472061873dd936ccb2805792
Byron Montaño <byronmb0407@gmail.com>
2024-05-31 22:13:20 -0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Food Premium',
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
